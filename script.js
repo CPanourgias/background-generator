1dd961dbcc0879705b672f99ba44042ef9a01a28
Panourgias Christos <panourgias.christos@gmail.com>
2019-07-25 10:09:41 +0300

    Added ES6 functions

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function hexToRgb(hex) {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result ? {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16)
-  } : null;
+const hexToRgb = (hex) => {
+	let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	return result ? {
+		r: parseInt(result[1], 16),
+		g: parseInt(result[2], 16),
+		b: parseInt(result[3], 16)
+	  } : null;
 }
 
-function componentToHex(c) {
-  var hex = c.toString(16);
+const componentToHex = (c) => {
+  let hex = c.toString(16);
   return hex.length == 1 ? "0" + hex : hex;
 }
 
-function rgbToHex(r, g, b) {return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);}
+const rgbToHex = (r, g, b) => "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 
 
 var color1 = document.querySelector("#color1");
